4dc5339782ce550dafc5f390354c310891fc492c
MathieuGood <bon.mathieu@gmail.com>
2026-02-10 23:23:48 +0100

    Added theme customization.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,11 +4,46 @@ import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
 import App from './App.vue'
 
+import { definePreset } from '@primevue/themes'
+
+const MidnightAmber = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '#fffbea',
+      100: '#fff3c4',
+      200: '#fde68a',
+      300: '#fcd34d',
+      400: '#fbbf24',
+      500: '#f59e0b',
+      600: '#d97706',
+      700: '#b45309',
+      800: '#92400e',
+      900: '#78350f',
+    },
+    secondary: {
+      50: '#f8fafc',
+      100: '#f1f5f9',
+      200: '#e2e8f0',
+      300: '#cbd5e1',
+      400: '#94a3b8',
+      500: '#64748b',
+      600: '#475569',
+      700: '#334155',
+      800: '#1e293b',
+      900: '#0f172a',
+    },
+  },
+})
 
 const app = createApp(App)
+
 app.use(PrimeVue, {
-    theme: {
-        preset : Aura
-    }
+  theme: {
+    preset: MidnightAmber,
+    options: {
+      //   darkModeSelector: '.dark',
+    },
+  },
 })
+
 app.mount('#app')
